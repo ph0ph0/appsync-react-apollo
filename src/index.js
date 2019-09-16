12ed4ad4d19c82e8c73940796248d261e07d1b05
ph0ph0 <phillipalanlee@googlemail.com>
2019-09-16 22:06:35 +0100

    c

--- src/index.js
+++ src/index.js
@@ -3,8 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom'
+import Amplify, { Auth } from 'aws-amplify'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+Amplify.configure({
+    Auth: {
+        // Required Amazon Cognito Identity Pool ID
+        identityPoolId: "xx-xxxx-x:",
+        // Required - Amazon Cognito Region
+        region: "XXXX",
+        //Optional - Amazon Cognito User Pool ID
+        userPoolId: "XXXXXX",
+        //Optional - Amazon Cognito Web Client ID
+        userPoolWebClientId: "xxxxxxx"
+
+    }
+})
+
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>, 
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
